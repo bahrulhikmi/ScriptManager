87b9da6c8ffb7ab6c3f32eee8b093759fc40b39a
eldchr <eldwinchristian95@gmail.com>
2021-10-14 15:03:53 +0700

    config reader

---
+++ ScriptManager/Reader/ConfigReader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Xml;
+using Common.Interfaces;
+
+namespace Reader
+{
+    public class ConfigReader : IConfigReader
+    {
+        public List<IConfigItem> Read()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)+ "\\System\\config.xml");
+            XmlElement root = doc.DocumentElement;
+            XmlNodeList nodes = root.ChildNodes;
+            var items = new List<IConfigItem>();
+            foreach(XmlNode node in nodes)
+            {
+                items.Add(new ConfigItem
+                {
+                    ConfigKey = node.Name,
+                    Value = node.InnerText,
+                    Type = "String"
+                });
+            }
+            return items;
+        }
+    }
+}
---
+++ ScriptManager/TestReader/Program.cs
@@ -0,0 +1,20 @@
+﻿using Reader;
+using System;
+
+namespace TestReader
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ConfigReader reader = new ConfigReader();
+            var x = reader.Read();
+            foreach(var a in x)
+            {
+                Console.WriteLine(a.ConfigKey + " " + a.Value);
+            }
+            Console.ReadLine();
+        }
+    }
+}
