109c3489ce0c9d33ca6d674e6c90b88a0f30f84b
iwayz <iway.sudiana@gmail.com>
2021-10-14 17:01:00 +0800

    Update form config

--- ScriptManager/SwissArmyKnife/FrmConfig.Designer.cs
+++ ScriptManager/SwissArmyKnife/FrmConfig.Designer.cs
@@ -30,21 +30,52 @@ namespace SwissArmyKnife
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmConfig));
+            this.propertyGrid = new System.Windows.Forms.PropertyGrid();
+            this.btnOK = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // propertyGrid
+            // 
+            this.propertyGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.propertyGrid.Location = new System.Drawing.Point(12, 12);
+            this.propertyGrid.Name = "propertyGrid";
+            this.propertyGrid.Size = new System.Drawing.Size(427, 426);
+            this.propertyGrid.TabIndex = 0;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOK.Location = new System.Drawing.Point(364, 444);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 1;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
             // 
             // FrmConfig
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(450, 492);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.propertyGrid);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "FrmConfig";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Configuration";
+            this.Load += new System.EventHandler(this.FrmConfig_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PropertyGrid propertyGrid;
+        private System.Windows.Forms.Button btnOK;
     }
 }
--- ScriptManager/SwissArmyKnife/FrmConfig.cs
+++ ScriptManager/SwissArmyKnife/FrmConfig.cs
@@ -16,5 +16,12 @@ namespace SwissArmyKnife
         {
             InitializeComponent();
         }
+
+        private void FrmConfig_Load(object sender, EventArgs e)
+        {
+            this.Text = "Swiss Army Knife Config " + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.Major.ToString() +
+                            "." + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.Minor.ToString() +
+                            "." + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.Build.ToString();
+        }
     }
 }
