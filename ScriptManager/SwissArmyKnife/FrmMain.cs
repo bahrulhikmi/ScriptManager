8d3aff905834818dcacee327493c4af3bd0e6901
arul <bahrulhikmi@gmail.com>
2022-03-11 15:38:50 +0800

    added definition loads

--- ScriptManager/SwissArmyKnife/FrmMain.cs
+++ ScriptManager/SwissArmyKnife/FrmMain.cs
@@ -1,20 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Common.Interfaces;
 
 namespace SwissArmyKnife
 {
     public partial class FrmMain : Form
     {
+        IConfigReader configReader;
+        IDefinitionReader definitionReader;
+        private List<IConfigItem> configurations;
+
         public FrmMain()
         {
             InitializeComponent();
+
+            //Could use Automapper here, but just instantiate it for now
+            definitionReader = new Reader.DefinitionReader();
+            configReader = new Reader.ConfigReader();
+
         }
 
         private void configToolStripMenuItem_Click(object sender, EventArgs e)
@@ -31,8 +35,32 @@ namespace SwissArmyKnife
         private void FrmMain_Load(object sender, EventArgs e)
         {
             this.Hide();
+            LoadConfig();
+            LoadDefinitionsAndPopulateUI();
         }
 
+
+        private void LoadConfig()
+        {
+           configurations = configReader.Read();
+        }
+
+        void LoadDefinitionsAndPopulateUI()
+        {
+            var definitions = definitionReader.Read();
+            cmsSwissArmyKnife.Items.Clear();
+            cmsSwissArmyKnife.Items.Add(configToolStripMenuItem);
+            cmsSwissArmyKnife.Items.Add(exitToolStripMenuItem);
+
+            foreach(var definition in definitions)
+            {
+                var item = new ToolStripMenuItem();
+                item.Text = definition.Name;
+                cmsSwissArmyKnife.Items.Add(item);
+            }
+
+        }
+   
         private void tesToolStripMenuItem_Click(object sender, EventArgs e)
         {
             //DefinitionItem defItem = new DefinitionItem();
